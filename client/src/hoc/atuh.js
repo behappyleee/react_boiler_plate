1ba9bc4135f8eedaae2aa1f96a7a833ae4df6fca
Chul Han Lee <dhfhfkwhdk@naver.com>
2021-10-31 21:45:58 +0900

    Project Commit

---
+++ client/src/hoc/atuh.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function (SpecificComponent, option, adminRoute = null) { 
+
+    function AuthenticaitonCheck(props) {
+
+        
+    }
+
+    return AuthenticaitonCheck;
+
+    
+}
+
