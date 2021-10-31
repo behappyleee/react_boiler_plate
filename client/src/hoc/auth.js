6ba9ae816e8550ab76c71a5b64c39f208b94f41d
Chul Han Lee <dhfhfkwhdk@naver.com>
2021-11-01 00:17:37 +0900

    Project Commit

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -11,16 +11,41 @@ export default function (SpecificComponent, option, adminRoute = null) {
 
         const dispatch = useDispatch();
 
+        // useEffect 는 기본 react class 형에서만 사용이 가능하였지만 그 기능을 함수형 에서도 사용가능하게 해주는 함수
         useEffect(() => {
-            
             dispatch(auth())
                 .then(response => {
-                console.log('auth.js userData : ', response)
-            
+                 console.log('auth.js userData : ', response)
+                // 로그인 하지 않은 상태
+                if(!response.payload.isAuth) {
+                    // 로그인 하지 않은 상태
+                    if(option) {
+                        props.history.push('/login');
+                    }
+                    // } else {
+                    //     // 로그인 한 상태 (admin 이 아니지만 admin페이지로 접근 시 )
+                    //     if(adminRoute && !response.data.isAdmin) {
+                    //         props.history.push('/');
+                    //     } else {
+                    //         // 로그인한 유저가 출입 불가능한 페이지로 이동 시 에
+                    //         if(option === false ) {
+                    //             props.history.push('/');
+                    //         }   
+                    //     }
+                    // }
+                } else {
+                    // 로그인 한 상태 (admin 이 아니지만 admin페이지로 접근 시 )
+                    if(adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/');
+                    } else {
+                        // 로그인한 유저가 출입 불가능한 페이지로 이동 시 에
+                        if(option === false ) {
+                            props.history.push('/');
+                        }   
+                    }
+                }
             })
-
            //  Axios.get('/api/users/auth' )
-        
         }, [])
         return (
             <SpecificComponent></SpecificComponent>
